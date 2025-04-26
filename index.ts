a628ca3c95e69cdc2a9ff2b9bedc3163e5704031
JunSoy <107024165+JunfelV@users.noreply.github.com>
2025-04-27 00:45:57 +0800

    not succesfully added in blokchain network because I have some issue sorry I late pass in the exame

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ async function main() {
     
 
     
-    const PRIVATE_KEY = new Ed25519PrivateKey("ed25519-priv-0x21849d02fe83b76ef0911c01451d2275d516048b623ef135c2ca65470e7e63f8"); // Replace with your private key
+    const PRIVATE_KEY = new Ed25519PrivateKey("ed25519-priv-0x7cf9a488636b2733e269919758ad5ef4c45caa66243fe691c3ec87cc6487475f"); // Replace with your private key
     const MY_ACCOUNT = Account.fromPrivateKey({ privateKey: PRIVATE_KEY });
     
     const myBalance = await aptos.getAccountAPTAmount({
@@ -26,17 +26,17 @@ async function main() {
       const transaction = await aptos.transaction.build.simple({
         sender: MY_ACCOUNT.accountAddress,
         data: {
-          function:
-            "0x777b93e13ff2a1bc872eb4d099ae15a52fb70f2f01dd18d7c809e217fb0e543e::tba_exam::add_participant",
+          function: "0x777b93e13ff2a1bc872eb4d099ae15a52fb70f2f01dd18d7c809e217fb0e543e::tba_exam::add_participant", 
           functionArguments: [
-            "0x539f880b3da2bc33d98b5efbf611eb76b6a980b0fdb15badb537767e0767d6e3",  // Replace with participant address
-            "Junfel Vargas Dalumpines",  // Replace with full name
-            "JunfelV",     // Replace with GitHub username
-            "junfeldalumpines@gmail.com",      // Replace with email address
-            "junfeld",    // Replace with Discord username
+            "0x43a01324effb79a2758497cf1c5858457a0ee80f6f9b2dcd428be76a6e9e89e3",  // Participant address (must be valid)
+            "Junfel Vargas Dalumpines",  // Full name of the participant
+            "JunfelV",                  // GitHub username
+            "junfeldalumpines@gmail.com",  // Email address
+            "junfeld",                  // Discord username
           ],
         },
       });
+      
       
     
       const senderAuthenticator = aptos.transaction.sign({
